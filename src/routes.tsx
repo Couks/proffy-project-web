c9ff05ecb5d2e4513b4aefd1996ba36b07930328
Matheus Castro <matheus.souza30@yahoo.com.br>
2020-08-05 22:38:49 -0300

    primeirocommit

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+/* Rota das páginas */
+
+import Landing from './pages/Landing';
+import TeacherForm from './pages/TeacherForm';
+import TeacherList from './pages/TeacherList';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" exact component={Landing}/>
+            <Route path="/study" component={TeacherList}/>
+            <Route path="/give-classes" component={TeacherForm}/>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
+
